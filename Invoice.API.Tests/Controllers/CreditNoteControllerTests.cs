44543d496cc4c7417ff1efb6fe3871526091c534
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2022-11-07 15:50:00 -0500

    Reduce cluttering V

--- Invoice.API.Tests/Controllers/CreditNoteControllerTests.cs
+++ Invoice.API.Tests/Controllers/CreditNoteControllerTests.cs
@@ -28,7 +28,7 @@ public class CreditNoteControllerTests
         _service.Setup(x => x.CreditNoteService.CreateCreditNoteAsync(It.IsAny<Guid>(), It.IsAny<NoteRequest>(), false)).ReturnsAsync(creditNoteResponse);
 
         //Act
-        var creditNoteController = new CreditNoteController(_service.Object);
+        var creditNoteController = new NoteController(_service.Object);
         var sut = await creditNoteController.CreateCreditNote(It.IsAny<Guid>(), creditNoteRequest);
 
         //Assert
@@ -44,8 +44,43 @@ public class CreditNoteControllerTests
         _service.Setup(x => x.CreditNoteService.GetCreditNoteAsync(debitNote.Id, false)).ReturnsAsync(debitNote);
 
         //Act
-        var creditNoteController = new CreditNoteController(_service.Object);
+        var creditNoteController = new NoteController(_service.Object);
         var sut = await creditNoteController.GetCreditNote(debitNote.Id);
+
+        //Assert
+        var actionResult = Assert.IsType<ActionResult<InvoiceResponse>>(sut);
+        var okObjectResult = Assert.IsType<OkObjectResult>(actionResult.Result);
+        var response = Assert.IsType<InvoiceResponse>(okObjectResult.Value);
+        Assert.Equal(response.Observations, debitNote.Observations);
+    }
+
+    [Fact]
+    public async Task DebitNoteController_CreateDebitNoteTest()
+    {
+        //Arrange
+        var debitNoteRequest = _fixture.Create<NoteRequest>();
+        var debitNoteResponse = _fixture.Create<InvoiceResponse>();
+        _service.Setup(x => x.DebitNoteService.CreateDebitNoteAsync(It.IsAny<Guid>(), It.IsAny<NoteRequest>(), false)).ReturnsAsync(debitNoteResponse);
+
+        //Act
+        var debitNoteController = new NoteController(_service.Object);
+        var sut = await debitNoteController.CreateDebitNote(It.IsAny<Guid>(), debitNoteRequest);
+
+        //Assert
+        var statusCodeResult = Assert.IsType<CreatedAtRouteResult>(sut);
+        Assert.Equal(201, statusCodeResult.StatusCode);
+    }
+
+    [Fact]
+    public async Task DebitNoteController_GetDebitNoteTest()
+    {
+        //Arrange
+        var debitNote = _fixture.Create<InvoiceResponse>();
+        _service.Setup(x => x.DebitNoteService.GetDebitNoteAsync(debitNote.Id, false)).ReturnsAsync(debitNote);
+
+        //Act
+        var debitNoteController = new NoteController(_service.Object);
+        var sut = await debitNoteController.GetDebitNote(debitNote.Id);
 
         //Assert
         var actionResult = Assert.IsType<ActionResult<InvoiceResponse>>(sut);
---
+++ Invoice.API/Controllers/NoteController.cs
@@ -0,0 +1,50 @@
+﻿using Invoice.API.ActionFilters;
+using Invoice.Service.Contracts.ServiceManagers;
+using Invoice.Shared.Request;
+using Invoice.Shared.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invoice.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NoteController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public NoteController(IServiceManager service) => _service = service;
+
+    [HttpPost("CreditNote/{issuerId:guid}")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> CreateCreditNote(Guid issuerId, NoteRequest request)
+    {
+        var debitNoteCreated = await _service.CreditNoteService.CreateCreditNoteAsync(issuerId, request, trackChanges: false);
+
+        return CreatedAtRoute("CreditNoteById", new { id = debitNoteCreated.Id }, debitNoteCreated);
+    }
+
+    [HttpGet("CreditNote/{id:guid}", Name = "CreditNoteById")]
+    public async Task<ActionResult<InvoiceResponse>> GetCreditNote(Guid id)
+    {
+        var creditNoteResponse = await _service.CreditNoteService.GetCreditNoteAsync(id, trackChanges: false);
+
+        return Ok(creditNoteResponse);
+    }
+
+    [HttpPost("DebitNote/{issuerId:guid}")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> CreateDebitNote(Guid issuerId, NoteRequest request)
+    {
+        var debitNoteCreated = await _service.DebitNoteService.CreateDebitNoteAsync(issuerId, request, trackChanges: false);
+
+        return CreatedAtRoute("DebitNoteById", new { id = debitNoteCreated.Id }, debitNoteCreated);
+    }
+
+    [HttpGet("DebitNote/{id:guid}", Name = "DebitNoteById")]
+    public async Task<ActionResult<InvoiceResponse>> GetDebitNote(Guid id)
+    {
+        var debitNoteResponse = await _service.DebitNoteService.GetDebitNoteAsync(id, trackChanges: false);
+
+        return Ok(debitNoteResponse);
+    }
+}
